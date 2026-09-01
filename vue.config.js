21e9581d05e24df048de1a5fa07d81e710e1707e
wangling <greybeard015@gmail.com>
2026-09-01 12:10:36 +0800

    build: add deploy.sh for gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // 部署到 https://<USERNAME>.github.io/<REPO>
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Furniture-ecommerce/'
+    : '/'
 })
